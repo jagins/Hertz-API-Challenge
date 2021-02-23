e16d9b897880ca78a83f3490b93166fe2d72e4aa
unknown <jagins629@gmail.com>
2021-02-23 15:49:51 -0600

    table is built for users

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.status(200).json({message: 'hello from users get request'});
 });
 
 router.post('/', (req, res) => {
