1b8617d251e4bc5a4dad39086e34d053ad3486f4
unknown <jagins629@gmail.com>
2021-02-25 15:05:21 -0600

    changed userId to be not nullable and unique and added check in middleware to see if the userId is in the body and check if it's a number

--- users/validateUser.js
+++ users/validateUser.js
@@ -2,17 +2,22 @@ const users = require('./users-model');
 
 function validateUser(req, res, next)
 {
-    const {userId} = req.body;
-    users.findUserByUserId(userId)
-    .then(foundUser => {
-        if(!foundUser) {
-            next();
-        }
-        else {
-            res.status(400).json({userRegistered: true, errorMessage: 'User has already been registered'})
-        }
-    })
-    .catch(err => res.status(500).json({error: 'Could retriveve user', errorMessage: err}))
+    if(req.body.userId && Number.isInteger(req.body.userId))
+    {
+        const {userId} = req.body;
+        users.findUserByUserId(userId)
+        .then(foundUser => {
+            if(!foundUser) {
+                next();
+            }
+            else {
+                res.status(400).json({userRegistered: true, errorMessage: 'User has already been registered'})
+            }
+        })
+        .catch(err => res.status(500).json({error: 'Could retriveve user', errorMessage: err}))
+    }
+    else
+        res.status(400).json({errorMessage: 'userId must be in the object set to an integer'})
 
 }
 
