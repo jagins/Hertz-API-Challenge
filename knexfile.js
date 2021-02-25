ff31e9634564461992c683f3fdc9300e597ce7b0
unknown <jagins629@gmail.com>
2021-02-25 15:27:10 -0600

    added dotenv and heroku pg database string

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,4 @@
-// Update with your config settings.
+require('dotenv').config()
 
 module.exports = {
 
@@ -23,5 +23,19 @@ module.exports = {
         }
     }
   },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  }
 
 };
